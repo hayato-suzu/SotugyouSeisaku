e97353ae81d2e996d02b84888dca74b4b3800f67
hayato0910 <hj-g19014@sist.ac.jp>
2021-11-18 09:46:05 +0900

    11/17prefabにBoxCollider追加、パズルスクリプトにプレハブ生成コード追加

--- ProtoTypeGame/Assets/Script/PuzzleScript.cs
+++ ProtoTypeGame/Assets/Script/PuzzleScript.cs
@@ -5,7 +5,29 @@ using UnityEngine.UI;
 
 public class PuzzleScript : MonoBehaviour
 {
+    //パネル格納変数
     [SerializeField] GameObject PuzzlePanel;
+    
+
+    [SerializeField]
+    [Tooltip("赤のプレハブ")]
+    public GameObject RedCube;
+
+    [SerializeField]
+    [Tooltip("青のプレハブ")]
+    public GameObject BlueCube;
+
+    [SerializeField]
+    [Tooltip("緑のプレハブ")]
+    public GameObject GreenCube;
+   
+    [SerializeField]
+    [Tooltip("生成する範囲A")]
+    public Transform RangeA;
+
+    [SerializeField]
+    [Tooltip("生成する範囲B")]
+    public Transform RangeB;
 
     public Text puzzlemessage;
 
@@ -30,7 +52,8 @@ public class PuzzleScript : MonoBehaviour
 
     public void RedButtonClick()
     {
-
+        
+        Instantiate(RedCube, new Vector3(1.0f, 1.0f, 122.0f), RedCube.transform.rotation);
     }
 
     public void BlueButtonClick()
