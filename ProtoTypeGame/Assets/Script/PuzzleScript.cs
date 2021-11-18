a2b50897f0be7819392d36bef8c36df0cbcb54be
hayato0910 <hj-g19014@sist.ac.jp>
2021-11-18 12:13:31 +0900

    11/18スクリプト追加、プレハブの修正、タグの追加、N1に追加

--- ProtoTypeGame/Assets/Script/PuzzleScript.cs
+++ ProtoTypeGame/Assets/Script/PuzzleScript.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 
 public class PuzzleScript : MonoBehaviour
 {
-    //パネル格納変数
-    [SerializeField] GameObject PuzzlePanel;
+    [SerializeField]
+    [Tooltip("パネル格納変数")]
+    GameObject PuzzlePanel;
     
-
     [SerializeField]
     [Tooltip("赤のプレハブ")]
     public GameObject RedCube;
@@ -29,49 +29,90 @@ public class PuzzleScript : MonoBehaviour
     [Tooltip("生成する範囲B")]
     public Transform RangeB;
 
+    //説明のテキスト
     public Text puzzlemessage;
+    //カウントテキスト
+    public Text CountText;
+
+    //生成した数のカウント
+     int CubeCount = 0;
+    //生成できる最大数
+    private int CubeCountMax = 2;
 
     private void Start()
     {
         PuzzlePanel.SetActive(false);
     }
 
+    private void Update()
+    {
+        //生成したときのテキスト更新
+        CountText.text = "あと" + (CubeCountMax - CubeCount) + "個";
+    }
+
     private void OnMouseDown()
     {
         //textを表示する
-        puzzlemessage.text = "あいうえお\naaa";
+        puzzlemessage.text = "二個まで生成できます";
+        CountText.text = "あと" + CubeCountMax + "個";
         PuzzlePanel.SetActive(true);
         //StartCoroutine("TextSet");
     }
 
-
+    //リセットボタン
     public void ResetButtonClick()
     {
-
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("CreateCube");
+        foreach(GameObject Cube in objects)
+        {
+            Destroy(Cube);
+            CubeCount = 0;
+        }
     }
-
+    //赤ボタン
     public void RedButtonClick()
     {
-        
-        Instantiate(RedCube, new Vector3(1.0f, 1.0f, 122.0f), RedCube.transform.rotation);
-    }
+        if (CubeCount < CubeCountMax)
+        {
+            float x = Random.Range(RangeA.position.x, RangeB.position.x);
+            float y = Random.Range(RangeA.position.y, RangeB.position.y);
+            float z = Random.Range(RangeA.position.z, RangeB.position.z);
 
+            Instantiate(RedCube, new Vector3(x, y, z), RedCube.transform.rotation);
+            CubeCount++;
+        }
+    }
+    //青ボタン
     public void BlueButtonClick()
     {
 
-    }
+        if (CubeCount < CubeCountMax)
+        {
+            float x = Random.Range(RangeA.position.x, RangeB.position.x);
+            float y = Random.Range(RangeA.position.y, RangeB.position.y);
+            float z = Random.Range(RangeA.position.z, RangeB.position.z);
 
+            Instantiate(BlueCube, new Vector3(x, y, z), BlueCube.transform.rotation);
+            CubeCount++;
+        }
+    }
+    //緑ボタン
     public void GreenButtonClick()
     {
+        if (CubeCount < CubeCountMax)
+        {
+            float x = Random.Range(RangeA.position.x, RangeB.position.x);
+            float y = Random.Range(RangeA.position.y, RangeB.position.y);
+            float z = Random.Range(RangeA.position.z, RangeB.position.z);
 
+            Instantiate(GreenCube, new Vector3(x, y, z), GreenCube.transform.rotation);
+            CubeCount++;
+        }
+    }
+    //パネルを消すボタン
+    public void BackButton()
+    {
+        PuzzlePanel.SetActive(false);
     }
 
-
-    //IEnumerator TextSet()
-    //{
-    //    //n秒後に消す
-    //    yield return new WaitForSeconds(5.0f);
-
-    //    puzzlemessage.text = "";
-    //}
 }
