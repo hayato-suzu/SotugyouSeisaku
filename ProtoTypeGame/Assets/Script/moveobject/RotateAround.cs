c1107803c187ba72c57b8a2ab772f458a33f878b
oobashuhe <hj-g19004@sist.ac.jp>
2021-12-06 14:48:20 +0900

    11/26　スクリプト変更(5秒ごとに90度一気に回転する床) 謎挙動するやつの復活

--- ProtoTypeGame/Assets/Script/moveobject/RotateAround.cs
+++ ProtoTypeGame/Assets/Script/moveobject/RotateAround.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class RotateAround : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    float seconds;
+    private void Update()
     {
-        //30•b‚Åˆê‰ñ“]
-        float rotateValue = 12 * Time.deltaTime;
-
-        transform.Rotate(new Vector3(0f, rotateValue, 0f));
+        seconds += Time.deltaTime;
+        if(seconds >= 5)
+        {
+            seconds = 0;
+            transform.Rotate(new Vector3(0, 90.0f, 0));
+        }
     }
 }
